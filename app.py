54b0932bece39065fea3f3254992d53d09e19418
Mario-S <spindizzy@outlook.com>
2017-02-25 15:46:49 +0100

    alias for api

--- app.py
+++ app.py
@@ -1,14 +1,13 @@
 import os
 
-from api.SkinCancerApi import SkinCancerApi
+from api.SkinCancerApi import SkinCancerApi as api
 
 #entry point to fire up the app
 def main():
     root_dir = os.path.dirname(os.path.realpath(__file__))
     port = 8000
 
-    app = SkinCancerApi(__name__,
-                        specification_dir=root_dir + '/swagger/',
+    app = api(__name__, specification_dir=root_dir + '/swagger/',
                         port=port,
                         api_file='skin-cancer-api.yaml')
     app.run()
